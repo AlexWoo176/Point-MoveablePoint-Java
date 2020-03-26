72fa085d0b746053752012468971b20ec0e030ff
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-26 17:40:42 +0700

    Complete

---
+++ src/classes/point/MoveAblePointTest.java
@@ -0,0 +1,8 @@
+package classes.point;
+
+public class MoveAblePointTest {
+    public static void main(String[] args) {
+        MoveAblePoint moveAblePoint = new MoveAblePoint(17.6f, 14.1f, 6.5f, 5.6f);
+        System.out.println(moveAblePoint);
+    }
+}
---
+++ src/classes/point/PointTest.java
@@ -0,0 +1,8 @@
+package classes.point;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point point = new Point(17.6f,14.1f);
+        System.out.println(point);
+    }
+}
